9800c4d7250f0441d21d6ad346944faf99e97934
TASTETH <88857872+TASTETH@users.noreply.github.com>
2024-10-17 16:37:02 +0300

    Add files via upload

---
+++ Pkmn123/src/main/java/ru/mirea/dzarasovra/pkmn/CardExport.java
@@ -0,0 +1,17 @@
+package ru.mirea.dzarasovra.pkmn;
+import java.io.*;
+public class CardExport {
+    private final Card card;
+
+    public CardExport(Card card) {
+        this.card = card;
+    }
+
+    public void serializeToBytes() throws IOException {
+        File output = new File("src/main/resources/" + this.card.getName() + ".crd");
+        try (FileOutputStream fileOutputStream = new FileOutputStream(output);
+             ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream)) {
+            objectOutputStream.writeObject(this.card);
+        }
+    }
+}
